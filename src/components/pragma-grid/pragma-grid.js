30d4fd6d1aa5487b74344258a984c172eabace8e
Mental Arena <caperaven@icloud.com>
2017-04-24 10:58:14 +0200

    loading asset data

--- src/components/pragma-grid/pragma-grid.js
+++ src/components/pragma-grid/pragma-grid.js
@@ -4,27 +4,18 @@ import {customElement, inject, bindable} from 'aurelia-framework';
 @inject(Element)
 export class PragmaGrid {
     @bindable columns;
-    @bindable sortOrder;
-    @bindable groupOrder;
-    @bindable items;
+    @bindable perspective;
 
     constructor(element) {
         this.element = element;
         this.columns = [];
-        this.sortOrder = [];
-        this.groupOrder = [];
     }
 
     attached() {
-        // initialize
     }
 
     detached() {
-        // dispose
         this.columns = null;
-        this.sortOrder = null;
-        this.groupOrder = null;
-        this.items = null;
     }
 }
 
--- src/views/grid-test/grid-test.js
+++ src/views/grid-test/grid-test.js
@@ -1,17 +1,54 @@
 import {bindable, inject} from "aurelia-framework";
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {GroupWorker, aggregates} from './../../lib/group-worker';
+import assets from './../../../../data/assets.json!text';
 
-@inject(EventAggregator)
+@inject(GroupWorker, EventAggregator)
 export class GridTest {
-    constructor(eventAggregator) {
+    gridOptions;
+    loading;
+
+    constructor(groupWorker, eventAggregator) {
         this.eventAggregator = eventAggregator;
+        this.groupWorker = groupWorker;
+        this.loading = false;
+        this.gridOptions = new GridOptions(null, ["parent_asset_id", "development_status"]);
     }
 
     attached() {
-        // initialize
+        this.handleDefaultAssetsHandler = this.handleDefaultAssets.bind(this);
+        this.assetsDefaultSubscription =  this.eventAggregator.subscribe("assets_default", this.handleDefaultAssetsHandler);
+
+        this.groupWorker.createCache("assets", JSON.parse(assets).data);
+        this.groupWorker.createGroupPerspective("assets", "default", this.gridOptions.groupOrder, { aggregate: aggregates.count });
+    }
+
+    handleDefaultAssets(args) {
+        console.log(args);
+        this.loading = false;
     }
 
     detached() {
-        // dispose
+        this.groupWorker.disposeCache("assets");
+        this.assetsDefaultSubscription.dispose();
+        this.assetsDefaultSubscription = null;
+        this.handleDefaultAssetsHandler = null;
+    }
+}
+
+class GridOptions {
+    columns;
+    sortOrder;
+    groupOrder;
+
+    /**
+     * constructor
+     * @param sortOrder: array of string, null if none
+     * @param groupOrder: array of sring, null if none
+     */
+    constructor(sortOrder, groupOrder) {
+        this.columns = [];
+        this.sortOrder = sortOrder;
+        this.groupOrder = groupOrder;
     }
 }
